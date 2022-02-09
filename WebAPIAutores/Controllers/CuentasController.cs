5e8266cc63fa157c414bb5a1efe2d996f3446336
alshira <alshira@gmail.com>
2022-02-09 02:52:05 -0600

    Registro susuarios API

---
+++ WebAPIAutores/Controllers/CuentasController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPIAutores.DTOs;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CuentasController: ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public CuentasController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("regsitrar")] //api/cuentas/registrar
+        public async Task<ActionResult<RespuestaAutenticacion>> Registrar(CredencialesUsuario credencialesUsuario)
+        {
+            var usuario = new IdentityUser { UserName = credencialesUsuario.Email, Email = credencialesUsuario.Email };
+            var resultado = await userManager.CreateAsync(usuario, credencialesUsuario.Password);
+            if (resultado.Succeeded)
+            {
+                return ConstruirToken(credencialesUsuario);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors);
+            }
+        }
+
+        private RespuestaAutenticacion ConstruirToken(CredencialesUsuario credencialesUsuario)
+        {
+            //vamos a regresar el token con el que se autentificará
+            var claims = new List<Claim>()
+                {
+                    //los claims son publicos y hasta el usuario las puede ver, no poner datos criticos TC, etc
+                    new Claim("email",credencialesUsuario.Email),
+                    new Claim("miCampo","mivalor")
+                };
+            var llave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["llavejwt"]));
+            var creds = new SigningCredentials(llave, SecurityAlgorithms.HmacSha256);
+            var expiracion = DateTime.UtcNow.AddDays(1);
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiracion, signingCredentials: creds);
+            return new RespuestaAutenticacion()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiracion = expiracion
+            }; 
+        }
+    }
+}
